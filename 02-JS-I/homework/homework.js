dddfc580b6e070052ef2f3f42a9e6d3a4fc39c1d
Confley25 <julio.martinez25@hotmail.com>
2021-12-05 14:49:12 +0100

    seguimos en proceso de JSI 1

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -109,6 +109,7 @@ function esPar(num) {
   // Devuelve "true" si "num" es par
   // De lo contrario, devuelve "false"
   // Tu código:
+    if (num %2 === 0) {return true;} else {return false;}
   
 }
 
@@ -116,19 +117,23 @@ function esImpar(num) {
   // Devuelve "true" si "num" es impar
   // De lo contrario, devuelve "false"
   // Tu código:
-  
+    if (num %2 === 1) {return true;} else {return false;}
+
 }
 
 function elevarAlCuadrado(num) {
   // Devuelve el valor de "num" elevado al cuadrado
   // ojo: No es raiz cuadrada!
   // Tu código:
+      return Math.pow(num, 2);
   
 }
 
 function elevarAlCubo(num) {
   // Devuelve el valor de "num" elevado al cubo
   // Tu código:
+
+    return Math.pow(num, 3);
   
 }
 
